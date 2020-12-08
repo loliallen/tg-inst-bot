c4d08d780962c85498bae08577cdae9f716964e5
loliallen <maxim.kasakin@yahoo.com>
2020-12-09 02:58:38 +0300

    upd dockerignore file

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,11 @@ const app = express()
 DataBase()
 
 app.listen(port, ()=>{
+    console.log(process.env['USER'])
+    console.log(process.env['PASSWORD'])
+    console.log(process.env['TELEGRAM_BOT_TOKEN'])
+    console.log(process.env['STEP_ID'])
+    console.log(process.env['MONGODB_URI'])
     const iBot = new InstagramBot()
     iBot.run()
     const tBot = new TelegramBot(iBot)
