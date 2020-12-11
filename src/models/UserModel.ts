e7055dcb8adfb0ce995b7db2c90436ac0d426b8a
loliallen <maxim.kasakin@yahoo.com>
2020-12-11 14:16:58 +0300

    testing unique validators

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -2,7 +2,15 @@ import { getModelForClass, prop, Ref } from "@typegoose/typegoose"
 import { Step } from "./StepModel";
 
 export class User {
-    @prop({ required: true, unique: true })
+    @prop({ 
+        required: true, 
+        unique: true,
+        set: async (value) => {
+            const u = await UserModel.findOne({ tg_id: value })
+            return !!u
+        }
+
+    })
     public tg_id!: string;
 
     @prop({ ref: () => Step })
